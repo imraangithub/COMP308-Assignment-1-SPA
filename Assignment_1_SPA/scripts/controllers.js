20cc598bac4349f92148118189ffc0ec815b0879
Adam Zhao <adamtieao@gmail.com>
2015-02-06 00:54:18 -0500

    Finish Search_3 - Filtering Repeaters

---
+++ Assignment_1_SPA/scripts/controllers.js
@@ -0,0 +1,29 @@
+﻿var phonecatApp = angular.module('phonecatApp', []);
+
+phonecatApp.controller('PhoneListCtrl', function ($scope) {
+    $scope.phones = [
+      {
+          'name': 'Nexus S',
+          'snippet': 'Fast just got faster with Nexus S.'
+      },
+      {
+          'name': 'Motorola XOOM™ with Wi-Fi',
+          'snippet': 'The Next, Next Generation tablet.'
+      },
+      {
+          'name': 'MOTOROLA XOOM™',
+          'snippet': 'The Next, Next Generation tablet.'
+      }
+    ];
+});
+
+describe('PhoneListCtrl', function () {
+
+    it('should create "phones" model with 3 phones', function () {
+        var scope = {},
+            ctrl = new PhoneListCtrl(scope);
+
+        expect(scope.phones.length).toBe(3);
+    });
+
+});
